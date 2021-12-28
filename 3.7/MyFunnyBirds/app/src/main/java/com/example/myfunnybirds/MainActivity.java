0c4ccc3b7f8164da8a1f5f052b33c801ef0e0f3e
Maslov Nikita <j-knaginino@yandex.ru>
2021-12-28 19:50:38 +0300

    FunnyBirds

---
+++ 3.7/MyFunnyBirds/app/src/main/java/com/example/myfunnybirds/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfunnybirds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GameView(this));
+    }
+}
