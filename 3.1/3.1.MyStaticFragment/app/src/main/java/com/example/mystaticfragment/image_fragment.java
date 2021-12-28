050072fc752f1a4760d7ef85e31175542a8170ab
Maslov Nikita <j-knaginino@yandex.ru>
2021-12-28 16:35:12 +0300

    3.1.1
    3.1.1

---
+++ 3.1/3.1.MyStaticFragment/app/src/main/java/com/example/mystaticfragment/image_fragment.java
@@ -0,0 +1,18 @@
+package com.example.mystaticfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class image_fragment extends Fragment {
+    @Override
+    // Переопределяем метод onCreateView
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.image_fragment, container, false);
+        return view;
+    }
+}
